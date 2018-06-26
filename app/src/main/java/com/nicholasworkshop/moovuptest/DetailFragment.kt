a34207e5324690183a2a57f4b54e7daf4f01d173
Nicholas Wong <nickwph@gmail.com>
2018-06-26 23:54:51 +0800

    Created fragment for detail view.

---
+++ app/src/main/java/com/nicholasworkshop/moovuptest/DetailFragment.kt
@@ -0,0 +1,75 @@
+package com.nicholasworkshop.moovuptest
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_detail.*
+
+class DetailFragment : Fragment() {
+
+//    private lateinit var mMap: GoogleMap
+
+    private val callback = MapReadyCallback()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail, container, false);
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(callback)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    class MapReadyCallback : OnMapReadyCallback {
+        override fun onMapReady(map: GoogleMap) {
+            val sydney = LatLng(-34.0, 151.0)
+            map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+            map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        }
+    }
+}
